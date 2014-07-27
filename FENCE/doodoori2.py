fadb75c5d105db84de7069c54258de2ef79a22c7
doodoori2 <doodoori2@gmail.com>
2014-07-27 19:06:10 +0800

    FENCE doodoori2.py

--- FENCE/doodoori2.py
+++ FENCE/doodoori2.py
@@ -14,37 +14,33 @@ else:
 
 class Fences():
     def __init__(self, fence_list):
-        #print fence_list
         self.fence_list_ = [int(x) for x in fence_list]
-        self.fence_list_len_ = len(fence_list)
 
-    def _divide(self, left, right):
-        lv = 0
-        rv = 0
+    def solve(self, left, right):
+        c_max = 0
         if left == right:
             return self.fence_list_[left]
         else:
             mid = int((right + left) / 2)
-            c_max = max(self._divide(left, mid), self._divide(mid + 1, right))
             lo = mid
             hi = mid+1
             height  = min(self.fence_list_[lo], self.fence_list_[hi])
-            c_max = max(c_max, height * 2)
-
+            c_max = max(c_max, self.solve(left, mid), self.solve(mid + 1, right), height * 2)
             while (left < lo or hi < right):
                 if (hi < right and (lo == left or self.fence_list_[lo-1] < self.fence_list_[hi+1])):
+                    hv = self.fence_list_[hi+1]
                     hi += 1
-                    height = min(height, self.fence_list_[hi])
+                    height = min(height, hv)
+                    while hi < right and hv <= self.fence_list_[hi+1]:
+                        hi += 1
                 else:
+                    lv = self.fence_list_[lo-1]
                     lo -= 1
-                    height = min(height, self.fence_list_[lo])
+                    height = min(height, lv)
+                    while lo > left and lv <= self.fence_list_[lo-1]:
+                        lo -= 1
                 c_max = max(c_max, height * (hi - lo + 1))
-
             return c_max
-
-    def getMaxArea(self):
-        return self._divide(0, len(self.fence_list_)-1)
-
 
 def main():
     n = int(rl())
@@ -52,7 +48,7 @@ def main():
         fence_cnt = int(rl())
         fence_input = rl().split()
         fences = Fences(fence_input)
-        print fences.getMaxArea()
+        print fences.solve(0, len(fences.fence_list_)-1)
 
 if __name__ == "__main__":
     main()
