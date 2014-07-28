53256c4ba9f374b4c454dc56c9a11bfe03331aba
ptc113 <ptc113@gmail.com>
2014-07-29 00:47:17 +0900

    1. PI 책 참고하여 수정하였음

--- PI/ptc113.cpp
+++ PI/ptc113.cpp
@@ -6,217 +6,82 @@
 //  Copyright (c) 2014년 PeterChoi. All rights reserved.
 //
 
-#include <iostream>
+// 1 2 3 4 5 / 6 7 8 9 10 11 12 13
 
-#define TEST_MODE false
+#include <iostream>
+#include <string.h>
+
+#define TEST_MODE true
 #if TEST_MODE
 #include <fstream>
-#include <string.h>
 #endif
 
 char strIn[10000+1];
+int  strMin[10000];
+int strSize;
+
 int ms; //minimum score
 int ts; //temp score
 
-enum pc{c1,c2,c3,c4,c5}; //part case
+enum {c1=1,c2=2,c3=4,c4=5,c5=10}; //part case
 
-pc findCase(const int ii)
+int findCase(const int ii, int nl)
 {
-    pc rr = c5;
+    int rr = c5;
     
-    if(strIn[ii+1] && strIn[ii+2])
+    if(strIn[ii] == strIn[ii+1] && strIn[ii] == strIn[ii+2])
     {
-        if(strIn[ii] == strIn[ii+1] && strIn[ii] == strIn[ii+2])
-        {
-            rr = c1;
-        }
-        else if(strIn[ii+1] - strIn[ii] == strIn[ii+2] - strIn[ii+1])
-        {
-            if(strIn[ii+1] - strIn[ii] == 1 || strIn[ii+1] - strIn[ii] == -1)
-                rr = c2;
-            else
-                rr = c4;
-        }
-        else if(strIn[ii] == strIn[ii+2])
-        {
-            rr = c3;
-        }
+        for(int i=3 ; i<nl ; i++)
+            if(strIn[ii] != strIn[ii+i])
+                return c5;
+        
+        rr = c1;
+        
     }
+    else if(strIn[ii+1] - strIn[ii] == strIn[ii+2] - strIn[ii+1])
+    {
+        int gap = strIn[ii+1] - strIn[ii];
+        
+        for(int i=3 ; i<nl ; i++)
+            if(strIn[ii+i] - strIn[ii+i-1] != gap)
+                return c5;
+        
+        if(gap == 1 || gap == -1)
+            rr = c2;
+        else
+            rr = c4;
+    }
+    else if(strIn[ii] == strIn[ii+2])
+    {
+        // 아오 모르겠다
+        if(nl >= 4 && strIn[ii+1] != strIn[ii+3]) return c5;
+        if(nl == 5 && strIn[ii+2] != strIn[ii+4]) return c5;
+        rr = c3;
+    }
+    
     return rr;
 }
 
 
-void find(const int ii)
+int find(const int ii)
 {
-    int t;
-    int tt;
+    if(ii >= strSize) return 0;
     
-    if(ms <= ts) return;
+    if(strMin[ii] != -1) return strMin[ii];
     
-    if(strIn[ii] == '\0')
+    int nMinSize = 987654321;
+    int nTemp;
+    
+    for(int i=3 ; i<= 5 ; i++)
     {
-        if( ms > ts)
-            ms = ts;
-        return;
-    }
-    
-    if(strIn[ii+1])
-    {
-        switch(findCase(ii))
+        if(ii+i<=strSize)
         {
-            case c1:
-                t = 1;
-                for(int i=1 ; i<5 + 3 ; i++)
-                {
-                    if(strIn[ii+i] && (strIn[ii] == strIn[ii+i])) t++;
-                    else break;
-                }
-                if( t >= 6)
-                {
-                    ts += 1;
-                    find(ii + t - 3); //6번 이상 페턴이 반복되면 n + 3 개로 나누는 것이 더 유리하기 때문
-                    ts -= 1;
-                }
-                else if( t >= 3)
-                {
-                    ts += 1;
-                    find(ii+t);
-                    ts -= 1;
-                }
-                break;
-            case c2:
-                t = 0;
-                tt = strIn[ii+1] - strIn[ii]; //속도를 위해 strIn[ii] - '0' 생략
-                
-                if(tt == 1 || tt == -1)
-                {
-                    t += 2;
-                    for(int i=1 ; i<4 + 3 ; i++)
-                    {
-                        if(strIn[ii+1+i] && (strIn[ii+1+i] - strIn[ii+i]) == tt) t++;
-                        else break;
-                    }
-                }
-                
-                //마지막에 한자리 때문에 다음 페턴이 어려워 지는 경우
-                if( t >= 4)
-                {
-                    if(findCase(ii + t - 1) < c2)
-                        t--;
-                }
-                
-                if( t >= 6)
-                {
-                    ts += 2;
-                    find(ii + t - 3); //6번 이상 페턴이 반복되면 n + 3 개로 나누는 것이 더 유리하기 때문
-                    ts -= 2;
-                }
-                else if(t >= 3)
-                {
-                    ts += 2;
-                    find(ii+t);
-                    ts -= 2;
-                }
-                break;
-            case c3:
-            {
-                t = 2;
-                char c1 = strIn[ii];
-                char c2 = strIn[ii+1];
-                
-                for(int i=2 ; i<5 + 3 ; i++)
-                {
-                    if( i % 2 == 0)
-                    {
-                        if(strIn[ii+i]&&strIn[ii+i] == c1) t++;
-                        else break;
-                    }
-                    else
-                    {
-                        if(strIn[ii+i]&&strIn[ii+i] == c2) t++;
-                        else break;
-                    }
-                }
-                //마지막에 한자리 때문에 다음 페턴이 어려워 지는 경우
-                if( t >= 4)
-                {
-                    if(findCase(ii + t - 1) < c3)
-                        t--;
-                }
-                if( t >= 6)
-                {
-                    ts += 4;
-                    find(ii + t - 3); //6번 이상 페턴이 반복되면 n + 3 개로 나누는 것이 더 유리하기 때문
-                    ts -= 4;
-                }
-                else if(t>=3)
-                {
-                    ts += 4;
-                    find(ii+t);
-                    ts -= 4;
-                }
-                break;
-            }
-            case c4:
-                t = 0;
-                tt = strIn[ii+1] - strIn[ii]; //속도를 위해 strIn[ii] - '0' 생략
-                //if(tt == 1 || tt == -1)
-                {
-                    t += 2;
-                    for(int i=1 ; i<4 + 3 ; i++)
-                    {
-                        if(strIn[ii+1+i]&&(strIn[ii+1+i] - strIn[ii+i]) == tt) t++;
-                        else break;
-                    }
-                }
-                //마지막에 한자리 때문에 다음 페턴이 어려워 지는 경우
-                if( t >= 4)
-                {
-                    if(findCase(ii + t - 1) < c4)
-                        t--;
-                }
-                if( t >= 6)
-                {
-                    ts += 5;
-                    find(ii + t - 3); //6번 이상 페턴이 반복되면 n + 3 개로 나누는 것이 더 유리하기 때문
-                    ts -= 5;
-                }
-                else if(t >= 3)
-                {
-                    ts += 5;
-                    find(ii+t);
-                    ts -= 5;
-                }
-                break;
-            case c5:
-                t = 1;
-                for(int i=1 ; i<5+3 ; i++)
-                {
-                    if(strIn[ii+i] && findCase(ii+i) == c5) t++;
-                    else break;
-                }
-                if( t>= 6)
-                {
-                    ts += 10;
-                    find(ii+t-3);
-                    ts -= 10;
-                }
-                else if( t>= 3)
-                {
-                    ts += 10;
-                    find(ii+t);
-                    ts -= 10;
-                }
-                else
-                {
-                    //이 케이스가 있는가?
-                    ts += 10;
-                    find(ii+t);
-                    ts -= 10;
-                }
-                break;
+            nTemp = find(ii+i) + findCase(ii, i);
+            nMinSize = (nMinSize > nTemp)?nTemp:nMinSize;
         }
     }
+    strMin[ii] = nMinSize;
+    return nMinSize;
 }
 
 
@@ -247,15 +112,18 @@ int main(int argc, const char * argv[])
 #else
         std::cin>>strIn;
 #endif
+        strSize = (int)strlen(strIn);
+        //find(0);
+        for(int i=0 ; i<strSize ; i++)
+            strMin[i] = -1;
         
-        find(0);
-        
+        ms = find(0);
         
         std::cout<<ms<<std::endl;
         
 #if TEST_MODE
-      if(r != ms)
-          std::cout<<"false"<<std::endl;
+        if(r != ms)
+            std::cout<<"false"<<std::endl;
 #endif
     }
 #if TEST_MODE
