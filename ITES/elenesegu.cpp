6ea99566ba84154a6d21c19366a4da39a75eb1b7
Elenesgu <elenesgu@gmail.com>
2014-07-30 23:44:08 +0900

    Add ITES/elenesgu.cpp
    어려워씀 ㅠㅠ

---
+++ ITES/elenesegu.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+#ifdef LOCAL
+#include<fstream>
+#include<chrono>
+#include<ctime>
+ifstream in("input.txt");
+ostream& out = cout;
+#else
+istream& in = cin;
+ostream& out = cout;
+#endif
+
+typedef pair<int, int> pint;
+typedef unsigned int num;
+
+const int MaxCase = 30000000;
+num Signals;
+
+short ConvertedSignals[MaxCase];
+
+void Init() {
+	Signals = 1983;
+	ConvertedSignals[0] = 1984;
+	for (int i = 1; i < MaxCase; i++) {
+		Signals = Signals * 214013 + 2531011;
+		ConvertedSignals[i] = Signals % 10000 + 1;
+	}
+}
+
+void ReInit(num b) {
+	for (int i = 0; i <= MaxCase - b; i++) {
+		ConvertedSignals[i] = ConvertedSignals[b + i];
+	}
+	for (int i = MaxCase - b + 1; i < MaxCase; i++) {
+		Signals = Signals * 214013 + 2531011;
+		ConvertedSignals[i] = Signals % 10000 + 1;
+	}
+}
+
+num ITES(const pint& obj);
+
+int main() {
+	size_t tCase;
+	pint tmp;
+	in >> tCase;
+	while (tCase--) {
+		Init();
+		in >> tmp.first >> tmp.second;
+		out << ITES(tmp) << endl;
+	}
+	return 0;
+}
+
+num ITES(const pint& obj) {
+	size_t begin = 0, end = 0, sigbegin = 0, con = MaxCase - sigbegin;
+	num sum = ConvertedSignals[begin];
+	num sol = 0;
+	while (end < obj.second) {
+		if (sum > obj.first) {
+			sum -= ConvertedSignals[begin++ - sigbegin];
+			if (begin > end) {
+				sum += ConvertedSignals[++end - sigbegin];
+			}
+		}
+		else if (sum < obj.first) {
+			sum += ConvertedSignals[++end - sigbegin];
+		}
+		else {
+			sol++;
+			sum += ConvertedSignals[++end - sigbegin];
+		}
+		if (sigbegin == 0 && end >= con) {
+			ReInit(begin);
+			sigbegin = begin;
+			con = MaxCase - sigbegin;
+		}
+	}
+	return sol;
+}
